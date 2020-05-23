a05b15424b66694d75f943c9c6dbb987a9e5291a
Marcle Rodrigues <maarclee@gmail.com>
2020-05-23 17:18:37 -0300

    Add Seats::Creator and Seats::Parser services

---
+++ app/services/seats/creator.rb
@@ -0,0 +1,45 @@
+require "best_seats/matrix"
+
+module Seats
+  class Creator
+    def self.call(params)
+      new(params).call
+    end
+
+    def initialize(params)
+      @venue_id = params.fetch(:venue_id)
+      @matrix_creator = params.fetch(:matrix_creator, ::BestSeats::Matrix)
+      @seats_parser = params.fetch(:seats_parser, ::Seats::Parser)
+    end
+
+    def call
+      matrix.each do |venue_row|
+        venue_row.each do |seat|
+          parsed_seat = seats_parser.call(seat)
+          Seat.create!(
+            row: parsed_seat.row,
+            column: parsed_seat.column, venue: venue
+          )
+        end
+      end
+    end
+
+    private
+
+    attr_reader :venue_id, :matrix_creator, :seats_parser
+
+    delegate :rows, :columns, to: :venue
+
+    def matrix
+      @_matrix = matrix_creator.new(
+        rows,
+        columns,
+        []
+      ).all
+    end
+
+    def venue
+      @_venue ||= Venue.find(venue_id)
+    end
+  end
+end
---
+++ spec/services/seats/creator_spec.rb
@@ -0,0 +1,63 @@
+require "rails_helper"
+
+RSpec.describe Seats::Creator do
+  describe ".call" do
+    context "with default matrix_creator and default seats parser" do
+      let(:venue) { create(:venue, rows: 2, columns: 2) }
+      let(:params) { { venue_id: venue.id } }
+
+      subject { described_class.call(params) }
+
+      it "creates the correct amount of seats" do
+        expect {
+          subject
+        }.to change(Seat, :count).from(0).to(4)
+      end
+
+      it "creates the correct seats" do
+        subject
+        seats = venue.reload.seats.pluck(:row, :column)
+        expected_seats = [
+          ['a', 1],
+          ['a', 2],
+          ['b', 1],
+          ['b', 2]
+        ]
+        expect(seats).to eq(expected_seats)
+      end
+    end
+
+    context "with custom matrix creator" do
+      let(:custom_matrix_creator) { double }
+      let(:venue) { create(:venue, rows: 2, columns: 2) }
+      let(:params) do
+        { venue_id: venue.id, matrix_creator: custom_matrix_creator }
+      end
+
+      subject { described_class.call(params) }
+
+      it "calls the correct methods in the customer matrix creator" do
+        expect(custom_matrix_creator).to receive(:new).with(2, 2, []).and_return(custom_matrix_creator)
+        expect(custom_matrix_creator).to receive(:all).and_return([])
+
+        subject
+      end
+    end
+
+    context "with custom seats parser" do
+      let(:custom_seats_parser) { double(row: 'a', column: 1) }
+      let(:venue) { create(:venue, rows: 2, columns: 2) }
+      let(:params) do
+        { venue_id: venue.id, seats_parser: custom_seats_parser }
+      end
+
+      subject { described_class.call(params) }
+
+      it "calls the correct methods in the customer matrix creator" do
+        expect(custom_seats_parser).to receive(:call).exactly(4).times.and_return(custom_seats_parser)
+
+        subject
+      end
+    end
+  end
+end
