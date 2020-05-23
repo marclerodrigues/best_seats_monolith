8d506b43d3f96aa88dfc007d45fb60035eb0c8e8
Marcle Rodrigues <maarclee@gmail.com>
2020-05-23 18:40:48 -0300

    Add new endpoints to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :venues, only: [:index, :create]
+    resources :venues, only: [:index, :create] do
+      member do
+        resources :seats, only: [:index, :update], param: :seat_id do
+          collection do
+            put :batches, to: "seats/batches#update", as: :batch_update
+          end
+        end
+      end
+    end
   end
 end
